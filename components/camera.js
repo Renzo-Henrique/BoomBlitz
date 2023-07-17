8de64f7da2c51e3fe4cc7adc686378d1f898f9ad
Renzo-Henrique <renzolealguzzo@gmail.com>
2023-07-16 22:55:08 -0300

    versão 1.2.0 e update readme

--- components/camera.js
+++ components/camera.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Board from './board/board';
 import styles from '../styles/Camera.module.css';
 import Debugger from './debugger/debugger'
-import PromptComponent from './prompt'
-import { openPrompt } from './promptUtils';
 /**
  * @brief Componente que representa a câmera do jogo.
  * @returns Div representando o tamanho do tabuleiro
@@ -15,8 +13,7 @@ function Camera (){
     <div className={styles.camera}>
       
       <Board/>
-      {<Debugger/> }
-      <PromptComponent openPrompt={openPrompt}/>
+      {/*<Debugger/>*/}
     </div>
   );
 };
--- components/promptUtils.js
+++
@@ -1,9 +0,0 @@
-export const openPrompt = () => {
-    return new Promise((resolve) => {
-      // Simula uma chamada assíncrona
-      setTimeout(() => {
-        const response = window.confirm('Pressione "OK" para Verdadeiro ou "Cancelar" para Falso.');
-        resolve(response);
-      }, 2000);
-    });
-  };
